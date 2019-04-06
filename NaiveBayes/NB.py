aa693615e37b80093c869ae207fe05d8363ecc5b
arshdeepdhillon <arshdeep.dhillon1@ucalgary.ca>
2019-04-05 18:36:19 -0600

    added indentation

--- NaiveBayes/NB.py
+++ NaiveBayes/NB.py
@@ -3,30 +3,30 @@ import zipfile
 with zipfile.ZipFile("reviews.zip", 'r') as zip_ref:
     zip_ref.extractall(".")
 
-#Get the label and reviews ready for processing
-with open("reviews.txt") as f:
-    reviews = f.read().split("\n")
-with open("labels.txt") as f:
-    labels = f.read().split("\n")
-labels = labels[:-1]
+    #Get the label and reviews ready for processing
+    with open("reviews.txt") as f:
+        reviews = f.read().split("\n")
+        with open("labels.txt") as f:
+            labels = f.read().split("\n")
+            labels = labels[:-1]
 
-reviews_tokens = [review.split() for review in reviews]
+            reviews_tokens = [review.split() for review in reviews]
 
-#Process data
-from sklearn.preprocessing import MultiLabelBinarizer
-onehot_enc = MultiLabelBinarizer()
-onehot_enc.fit(reviews_tokens)
+            #Process data
+            from sklearn.preprocessing import MultiLabelBinarizer
+            onehot_enc = MultiLabelBinarizer()
+            onehot_enc.fit(reviews_tokens)
 
-#Split data for training and testing
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(reviews_tokens, labels, test_size=0.25, random_state=None)
+            #Split data for training and testing
+            from sklearn.model_selection import train_test_split
+            X_train, X_test, y_train, y_test = train_test_split(reviews_tokens, labels, test_size=0.25, random_state=None)
 
-#Get ML algorithm to train
-from sklearn.naive_bayes import BernoulliNB
+            #Get ML algorithm to train
+            from sklearn.naive_bayes import BernoulliNB
 
-bnbc = BernoulliNB(binarize=None)
-bnbc.fit(onehot_enc.transform(X_train), y_train)
+            bnbc = BernoulliNB(binarize=None)
+            bnbc.fit(onehot_enc.transform(X_train), y_train)
 
-#Get score
-score = bnbc.score(onehot_enc.transform(X_test), y_test)
-print(score)
+            #Get score
+            score = bnbc.score(onehot_enc.transform(X_test), y_test)
+            print(score)
